9aeaa4ded7ea71887675c41d7df20c4adac36d81
Aashish Kiran <aashish.kiran@gmail.com>
2018-03-20 00:18:59 +0530

    added form validation

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CONTACTS } from '../mock-contacts';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacts',
@@ -8,12 +9,39 @@ import { CONTACTS } from '../mock-contacts';
 })
 export class ContactsComponent implements OnInit {
   contacts = CONTACTS;
+  contact: any = {};
+  contactForm;
+  //This will hide the DIV by default.
+  is_visible: boolean = false;
 
-
-
-  constructor() { }
-
-  ngOnInit() {
+  //If DIV is visible it will be hidden and vice versa.
+  toggle() {
+    this.is_visible = !this.is_visible;
   }
 
+  //Add new contact
+  add() {
+    if (this.contact.type != null) {
+      this.contacts.push(this.contact)
+      this.contact = {};
+    }
+  }
+
+  //delete contact
+  delete(index) {
+    if (index > -1) {
+      this.contacts.splice(index, 1);
+    }
+  }
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.contactForm = new FormGroup ({
+      type: new FormControl(this.contact.type, Validators.required)
+    });
+  }
+
+get type() { return this.contactForm.get('type'); }
 }
